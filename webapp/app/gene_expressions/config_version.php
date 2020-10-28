9c06ef1ba40bf400ad0c383db17a700b0b5ef3cc
Derrick Cheng <derrick@bioinforx.com>
2020-10-27 20:20:00 -0500

    Upload

---
+++ webapp/app/gene_expressions/config_version.php
@@ -0,0 +1,7 @@
+<?php
+
+$APP_CONFIG['Version']['Application Version'] 		= '2020-10-22';
+$APP_CONFIG['Version']['Database Definition'] 		= '2020-05-15';
+$APP_CONFIG['Version']['Cache Version']		 		= '2020-10-22';
+
+?>
---
+++ webapp/bxaf_setup/config.php
@@ -0,0 +1,9 @@
+<?php
+
+include_once('default.php');
+
+if (is_file('override.php')){
+	include_once('override.php');
+}
+
+?>
